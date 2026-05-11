428e79fc315d2bc28af994b96996a75ee9e76834
Julia Kraysha <Julia Kraysha@JULIA-SAMA>
2026-05-11 23:59:02 +0800

    added ordering system models

--- MakeUBeauty/Models/Order.cs
+++ MakeUBeauty/Models/Order.cs
@@ -1,22 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MakeUBeauty.Models
 {
     public class Order
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public int UserId { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string FullName { get; set; }
+
+        [Required]
         public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
         public string Address { get; set; }
+
         public string ShippingMethod { get; set; }
+
         public string PaymentMethod { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal TotalAmount { get; set; }
-        public DateTime OrderDate { get; set; }
-        public string Status { get; set; }
+
+        public DateTime OrderDate { get; set; } = DateTime.Now;
+
+        public string Status { get; set; } = "Pending";
+
+        [ForeignKey("UserId")]
         public virtual User User { get; set; }
-        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+        public virtual List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
     }
 }
--- MakeUBeauty/Models/OrderItem.cs
+++ MakeUBeauty/Models/OrderItem.cs
@@ -1,15 +1,34 @@
-﻿namespace MakeUBeauty.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MakeUBeauty.Models
 {
     public class OrderItem
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public int OrderId { get; set; }
+
+        [Required]
         public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
         public virtual Product Product { get; set; }
 
+        [Required]
         public string ProductName { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
+        [Range(1, int.MaxValue)]
         public int Quantity { get; set; }
+
         public string ImageUrl { get; set; }
+
+        [ForeignKey("OrderId")]
+        public virtual Order Order { get; set; }
     }
 }
