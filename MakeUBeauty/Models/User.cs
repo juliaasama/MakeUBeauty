0f168f3c3e0f770d18ade352c7eb0daf65948343
Julia Kraysha <Julia Kraysha@JULIA-SAMA>
2026-05-11 23:56:30 +0800

    core identify entities

--- MakeUBeauty/Models/User.cs
+++ MakeUBeauty/Models/User.cs
@@ -1,17 +1,40 @@
-﻿namespace MakeUBeauty.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MakeUBeauty.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(100)]
         public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; } = string.Empty;
+
         public string? PhoneNumber { get; set; }
+
         public string? Gender { get; set; }
+
         public string? Bio { get; set; }
+
         public string? ProfilePicture { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
         public string Password { get; set; } = string.Empty;
-        public string? Role { get; set; }
+
+        public string? Role { get; set; } = "Customer";
+
+        [DataType(DataType.Date)]
         public DateTime? Birthday { get; set; }
+
         public virtual ICollection<Product> Products { get; set; } = new List<Product>();
     }
 }
