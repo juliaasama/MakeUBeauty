2c2536a9e25f8d56233e1070a8433dcede735ad4
Julia Kraysha <Julia Kraysha@JULIA-SAMA>
2026-05-11 23:58:30 +0800

    modified cart and wishlist

--- MakeUBeauty/Models/Wishlist.cs
+++ MakeUBeauty/Models/Wishlist.cs
@@ -1,10 +1,23 @@
-﻿namespace MakeUBeauty.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MakeUBeauty.Models
 {
     public class Wishlist
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
         public int ProductId { get; set; }
+
+        [Required]
         public int UserId { get; set; }
-        public Product? Product { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual Product? Product { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User? User { get; set; }
     }
 }
